9f46e5a076149add251f2de548d74241ff6a1ebd
lianxiaopeng <536545214@qq.com>
2015-09-17 22:11:39 +0800

    星期四

---
+++ python/hello8.py
@@ -0,0 +1,42 @@
+#装饰器作业
+
+import functools
+from collections import Iterable
+def log(p1):
+	#print("log")
+	#print(p1)
+	
+	def log_f1(p1):
+		#print("log_f1")
+		#print(p1)
+		@functools.wraps(p1)
+		def log_f2(*args):
+			print("log_f2")
+			print(args)
+			p1()
+			return 1
+		return log_f2
+	@functools.wraps(p1)
+	def log_f3(*args):
+		print("log_f3")
+		print(args)
+		p1()
+		return 2
+	if isinstance(p1,Iterable):
+		return log_f1
+	else:
+		return log_f3
+
+@log("sss")
+def f1():
+	print("f1")
+@log
+def f2():
+	print("f1")
+
+#f1 =  log("sss")(f1)
+f1("abc")
+print(f1.__name__)
+f2("cba")
+print(f2.__name__)
+
---
+++ python/hello9.py
@@ -0,0 +1,13 @@
+#偏函数
+b = int("01011001",base=2)
+print(b)
+def int2(b) :
+	return int(b,base=2)
+print(int2("111100"))
+
+#简化调用
+import functools
+int8 =  functools.partial(int,base=8)
+print(int8("71"))
+max10 = functools.partial(max,10)
+print(max10(5,6,7))
